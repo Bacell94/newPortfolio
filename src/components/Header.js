5d1745d337762264ea2c3fae99eb614657977123
Bassel saleh <bacell94@gmail.com>
2022-02-09 19:10:58 +0200

    starting with header and nav

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+export default function Header() {
+    return (
+        <header>
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+            <button className="nav-toggle" aria-label="nav toggle"></button>
+            <nav className="nav">
+                <ul className="nav__list">
+                    <li className="nav__item"><a href="http://"></a></li>
+                    <li className="nav__item"><a href="http://"></a></li>
+                    <li className="nav__item"><a href="http://"></a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
