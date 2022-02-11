410f665e8daab718d8a0af15b5ab39693c8c7558
Bassel saleh <bacell94@gmail.com>
2022-02-11 19:40:46 +0200

    added footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,27 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodepen, faDribbble, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
     return (
         <footer>
             <a href="mailto:" class="footer__link">e-mail</a>
             <ul class="social-list">
-                <li class="social-list__item"><a class="social-list__link" href="https://codepen.io">a</a></li>
-                <li class="social-list__item"><a class="social-list__link" href="http://dribbble.com">b</a></li>
-                <li class="social-list__item"><a class="social-list__link" href="https://twitter.com">c</a></li>
-                <li class="social-list__item"><a class="social-list__link" href="https://github.com">d</a></li>
+                <li class="social-list__item">
+                    <a class="social-list__link" href="https://codepen.io">
+                    <FontAwesomeIcon icon={faCodepen} />
+                    </a></li>
+                <li class="social-list__item">
+                    <a class="social-list__link" href="http://dribbble.com">
+                    <FontAwesomeIcon icon={faDribbble} />
+                    </a></li>
+                <li class="social-list__item">
+                    <a class="social-list__link" href="https://twitter.com">
+                    <FontAwesomeIcon icon={faTwitter} />
+                    </a></li>
+                <li class="social-list__item">
+                    <a class="social-list__link" href="https://github.com">
+                    <FontAwesomeIcon icon={faGithub} />
+                    </a></li>
             </ul>
         </footer>
     )
