aff67cca5d7b6d10b95fe03ed4100f54d9d5b98d
Bassel saleh <bacell94@gmail.com>
2022-02-11 14:41:01 +0200

    aboutme css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
             <h2 className="section__title section__title__about">Who i am</h2>
             <p className="section__subtitle section__subtitle--about">designer and developer</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <img src={devJane2} alt="" />
+            <img className="about-me__img" src={devJane2} alt="" />
         </section>
     )
 }
