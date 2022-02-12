69aa6a4831f4214ebb1a1b67bbe63c07ae466687
Bassel saleh <bacell94@gmail.com>
2022-02-12 12:54:40 +0200

    hamburger css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import react from "react";
 import Header from "./components/Header";
 import Introduction from "./components/Introduction";
 import MyServices from "./components/MyServices";
@@ -6,9 +7,14 @@ import MyWork from "./components/MyWork";
 import Footer from "./components/Footer";
 
 export default function App() {
+    const [navOn, setNavOn] = react.useState(false)
+
+    function handleClick() {
+        setNavOn(prev => !prev)
+    }
     return (
         <div>
-            <Header />
+            <Header click={handleClick} nav={navOn}/>
             <Introduction />
             <MyServices />
             <AboutMe />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import logo from "../images/devjane.png"
-export default function Header() {
+export default function Header(props) {
     return (
         <header>
             <div className="logo">
                 <img src={logo} alt="" />
             </div>
-            <button className="nav-toggle" aria-label="nav toggle">
-                <span className="hamburger"></span>
+            <button className="nav-toggle" aria-label="nav toggle" onClick={props.click}>
+                <span className={`hamburger ${props.nav && "hamburger-on"}`} ></span>
             </button>
-            <nav className="nav">
+            <nav className={`nav ${props.nav && "nav-on"}`}>
                 <ul className="nav__list">
-                    <li className="nav__item"><a className="nav__link" href="#home">Home</a></li>
-                    <li className="nav__item"><a className="nav__link" href="#services">My services</a></li>
-                    <li className="nav__item"><a className="nav__link" href="#about">About me</a></li>
-                    <li className="nav__item"><a className="nav__link" href="#work">My work</a></li>
+                    <li className="nav__item">
+                        <a className="nav__link" href="#home" onClick={props.click}>Home</a></li>
+                    <li className="nav__item">
+                        <a className="nav__link" href="#services" onClick={props.click}>My services</a></li>
+                    <li className="nav__item">
+                        <a className="nav__link" href="#about" onClick={props.click}>About me</a></li>
+                    <li className="nav__item">
+                        <a className="nav__link" href="#work" onClick={props.click}>My work</a></li>
                 </ul>
             </nav>
         </header>
