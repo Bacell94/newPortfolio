5ea6eefd66ff04efea05b27bd9b22789cb7e90e0
Bassel saleh <bacell94@gmail.com>
2022-03-10 12:47:01 +0200

    functional form wit emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import emailjs from 'emailjs-com';
+
 export default function Contact() {
 
     const [name, setName] = useState('');
@@ -6,10 +8,22 @@ export default function Contact() {
     const [message, setMessage] = useState('');
     const [emailSent, setEmailSent] = useState(false);
 
-    const submit = () => {
+    const submit = (e) => {
+        e.preventDefault();
         if (name && email && message) {
-           // TODO - send mail
-    
+           const serviceId = 'service_gtky4bk';
+           const templateId = 'template_0c3hp8j';
+           const userId = 'oQvPJAXGzqcHpSDcR';
+           const templateParams = {
+               name,
+               email,
+               message
+           };
+
+           emailjs.send(serviceId, templateId, templateParams, userId)
+               .then(response => console.log(response))
+               .then(error => console.log(error));
+
             setName('');
             setEmail('');
             setMessage('');
@@ -21,12 +35,12 @@ export default function Contact() {
 
     return(
         <section className="contact" id="contact">
-            <form action="">
+            <form  onSubmit={submit}>
                 <h2 class="section-title__contact">How can i help?</h2>
-                    <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
-                    <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)}/>
-                    <textarea name="" id="" cols="30" rows="10" placeholder="your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
-                    <button className="btn" onClick={submit}>Send Message</button>
+                    <input name="email" type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input name="name" type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)}/>
+                    <textarea name="message" id="" cols="30" rows="10" placeholder="your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                    <button className="btn">Send Message</button>
                     <span className={!emailSent ? 'hidden' : null}>Thank you for your message, we will be in touch in no time!</span>
                 </form>
         </section>
